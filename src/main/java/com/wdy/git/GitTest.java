4aa54089a407355719e19e4e19208e6b36df6347
wdy <1593286801@qq.com>
2022-08-22 11:18:03 +0800

    first commit

---
+++ src/main/java/com/wdy/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.wdy.git;
+
+/**
+ * @author wdy
+ * @create 2022-08-22-11:06
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
